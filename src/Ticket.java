968e17c7f16b3f28008cd0e72d275d492af5c2fa
archerarcher <tranx699@umn.edu>
2018-07-24 14:12:08 -0500

    Create getters and setters for the Ticket class. Make instance variables private.

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,8 +1,14 @@
 import java.util.Arrays;
 
+/**
+ * Ticket
+ *
+ * This class is used to represent each ticket that is created for an
+ * event.
+ */
 public class Ticket {
-    byte[] hash;    // The hash value of the ticket
-    boolean used;   // Whether the ticket has been used yet
+    private final byte[] hash;    // The hash value of the ticket
+    private boolean used;   // Whether the ticket has been used yet
 
     public Ticket(byte[] hash_input) {
         hash = hash_input;
@@ -16,4 +22,16 @@ public class Ticket {
         }
         return false;
     }
+
+
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    // We don't need Setters since the hash value of a ticket should never change and
+    // the ticket being used only happens when we check to validate it.
 }
